079d90a7f71e3d9ce68c957d227787817a5769d1
Gabriel Lindström <gabriel.lindstrom@gmail.com>
2019-11-24 20:03:08 +0200

    Exercise 4.5 done

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,8 +1,20 @@
 const dummy = blogs => 1
-
 const totalLikes = blogs => blogs.reduce((sum, blog) => sum + blog.likes, 0)
+const favoriteBlog = blogs => blogs.reduce((favorite, blog) => {
+    if (!favorite.likes || blog.likes > favorite.likes) {
+        return {
+            title: blog.title,
+            author: blog.author,
+            likes: blog.likes
+        }
+    } else {
+        return favorite
+    }
+}, {})
+
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }

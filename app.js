c377b0d2976768d250ae5a39996df5ae9aeb81d3
Gabriel Lindström <gabriel.lindstrom@gmail.com>
2019-11-24 09:51:02 +0200

    Exercise 4.2 done

--- app.js
+++ app.js
@@ -4,26 +4,12 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
-const Blog = require('./models/blog')
+const blogsRouter = require('./controllers/blogs')
 
 mongoose.connect(config.MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true })
 
 app.use(cors())
 app.use(bodyParser.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then(blogs => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then(result => {
-    response.status(201).json(result)
-  })
-})
-
+app.use('/api/blogs', blogsRouter)
 
 module.exports = app

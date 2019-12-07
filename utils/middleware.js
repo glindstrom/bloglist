a4eda0cab34cbd1fa0a00102caeaba5921849da4
Gabriel Lindström <gabriel.lindstrom@gmail.com>
2019-12-07 21:00:26 +0200

    Exercise 4.20 done

--- utils/middleware.js
+++ utils/middleware.js
@@ -7,6 +7,20 @@ const errorHandler = (error, request, response, next) => {
   next(error)
 }
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
+const tokenExtractor = (request, response, next) => {
+  request.token = getTokenFrom(request)
+  next()
+}
+
 module.exports = {
-  errorHandler
+  errorHandler,
+  tokenExtractor
 }
